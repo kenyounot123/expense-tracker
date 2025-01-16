cae144cc68374856ec3cc3e317d536310341ffd5
kenyounot123 <kenlu519@gmail.com>
2025-01-15 21:14:29 -0500

    Fix brakeman warnings, use view helpers instead of send method

--- app/helpers/dashboard_analytics_helper.rb
+++ app/helpers/dashboard_analytics_helper.rb
@@ -1,10 +1,26 @@
 module DashboardAnalyticsHelper
+  VALID_CHART_TYPES = %w[income spendings profits].freeze
+
   def format_analytics_type(type)
     case type.to_sym
     when :spendings
       "Spent"
     else
       type.to_s.titleize
+    end
+  end
+
+  def chart_path_for(type)
+    type = (type || "spendings").to_s
+    type = "spendings" unless VALID_CHART_TYPES.include?(type)
+
+    case type
+    when "income"
+      charts_income_path
+    when "spendings"
+      charts_spendings_path
+    when "profits"
+      charts_profits_path
     end
   end
 end
