cd12da92eac9835d4dd8b5d6f01e8106ed850286
Ken Lu <kenlu519@gmail.com>
2025-01-26 22:18:04 -0500

    update to mission control to use default authentication

--- app/controllers/admin_controller.rb
+++
@@ -1,4 +0,0 @@
-class AdminController < ActionController::Base
-  http_basic_authenticate_with name: Rails.application.credentials.admin.username,
-                             password: Rails.application.credentials.admin.password
-end
