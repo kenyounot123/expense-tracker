7e8c1b8bfd7cf146af0b86b54e31fc416bb0015b
Ken Lu <kenlu519@gmail.com>
2025-01-26 22:11:01 -0500

    use credentials.dig to get admin credentials

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,3 +1,4 @@
 class AdminController < ActionController::Base
-  http_basic_authenticate_with name: Rails.application.credentials.admin.username, password: Rails.application.credentials.admin.password
+  http_basic_authenticate_with name: Rails.application.credentials.admin.username,
+                             password: Rails.application.credentials.admin.password
 end
