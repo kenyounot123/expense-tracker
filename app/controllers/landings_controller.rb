9b68701e0d30d0e96439099876c78d62ff476d26
Ken Lu <kenlu519@gmail.com>
2025-09-21 16:03:41 -0400

    refactor: update landing page and add google form

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -2,6 +2,7 @@ class LandingsController < ApplicationController
   allow_unauthenticated_access
   layout "landing"
   def index
+    redirect_to expenses_path if authenticated?
   end
 
   def privacy
