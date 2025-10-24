34853a2de0ddeed13f5e70109aca3f5277a13b56
Ken Lu <kenlu519@gmail.com>
2025-10-24 00:38:19 -0400

    fix: add concurrent algorithm to prevent db locks

--- db/migrate/20251024041904_update_category_name_uniqueness_index.rb
+++ db/migrate/20251024041904_update_category_name_uniqueness_index.rb
@@ -1,9 +1,11 @@
 class UpdateCategoryNameUniquenessIndex < ActiveRecord::Migration[8.0]
+  disable_ddl_transaction!
+
   def change
     # Remove the global unique index on name
-    remove_index :categories, :name, if_exists: true
+    remove_index :categories, :name, if_exists: true, algorithm: :concurrent
 
     # Add a composite unique index on name scoped to user_id
-    add_index :categories, [ :user_id, :name ], unique: true
+    add_index :categories, [ :user_id, :name ], unique: true, algorithm: :concurrent
   end
 end
